4d0926a44cea05536d60d6cef326009d5c79e66b
Ismael Sebastián Andrango Peralbo <51093008+SebastianAndrango@users.noreply.github.com>
2023-09-29 10:21:58 -0500

    Fix bug logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { LoginService } from './../../services/login.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -12,7 +13,7 @@ export class NavbarComponent implements OnInit {
   isLoggedIn = false;
   user:any = null;
 
-  constructor(public login:LoginService) { }
+  constructor(public login:LoginService, private router: Router) { }
 
   ngOnInit(): void {
     this.isLoggedIn = this.login.isLoggedIn();
@@ -27,6 +28,8 @@ export class NavbarComponent implements OnInit {
 
   public logout(){
     this.login.logout();
+    //window.location.reload();
+    this.router.navigate(['']);
     window.location.reload();
   }
 
